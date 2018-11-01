0b78e1d3f241df332d573d4410d08966fa511653
XuRongDong <3003154880@qq.com>
2018-11-01 20:24:06 +0800

    add new style

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,65 +1,47 @@
 <template>
-  	<div class="login_page fillcontain">
-	  	<transition name="form-fade" mode="in-out">
-	  		<section class="form_contianer" v-show="showLogin">
-		  		<div class="manage_tip">
-		  			<p>教务系统</p>
-		  		</div>
-		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
-					<el-form-item prop="username">
-						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input type="password" @keyup.enter.native="submitForm('loginForm')" placeholder="密码" v-model="loginForm.password"></el-input>
-					</el-form-item>
-					<el-form-item>
-				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
-				  	</el-form-item>
-				</el-form>
-				<p class="tip">温馨提示：</p>
-				<p class="tip">操作完毕后记得退出账户.</p>
-	  		</section>
-	  	</transition>
-  	</div>
+  <div class="outer">
+    <div class="middle">
+      <div class="inner">
+        <el-form>
+          <el-form-item prop="username">
+            <el-input placeholder="用户名"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input type="password" placeholder="密码"></el-input>
+          </el-form-item>
+          <el-form-item style="text-align:center">
+            <el-button type="primary" class="submit_btn">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import { mapActions, mapState } from "vuex";
-import utils from "@/utils/index.js";
-import * as CONFIG from "@/config/index.js";
-export default {
-  data() {
-    return {
-      loginForm: {
-        username: "",
-        password: ""
-      },
-      rules: {
-        username: [
-          { required: true, message: "请输入用户名", trigger: "blur" }
-        ],
-        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
-      },
-      showLogin: false
-    };
-  },
-  mounted() {
-    this.showLogin = true;
-  },
-  methods: {
-    async submitForm(formName) {
-      let res = await utils.simpleGet(
-        `${CONFIG.BASE_URL}/user/login`,
-        this.loginForm
-      );
+<style>
+.outer {
+  display: table;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  /*
+   * code below generate the background image
+   * https://stackoverflow.com/questions/3540194/how-to-make-a-grid-like-graph-paper-grid-with-just-css
+   * http://codepen.io/anon/pen/VvPWvv
+   */
+  background-size: 40px 40px;
+  background-image: linear-gradient(to right, grey 1px, transparent 1px),
+    linear-gradient(to bottom, grey 1px, transparent 1px);
+}
 
-      if (res.code === 0) {
-        this.route("voteSuccess");
-      } else {
-        alert(res.msg || "请求失败");
-      }
-    }
-  },
-};
-</script>
+.middle {
+  display: table-cell;
+  vertical-align: middle;
+}
 
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 20%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,48 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import login from "@/page/login.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: login
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            redirect: '/dashboard'
+        },
+        {
+            path: '/',
+            component: resolve => require(['../components/common/Home.vue'], resolve),
+            meta: { title: '自述文件' },
+            children:[
+                {
+                    path: '/dashboard',
+                    component: resolve => require(['../components/page/Dashboard.vue'], resolve),
+                    meta: { title: '系统首页' }
+                },
+                {
+                    path: '/teacherList',
+                    component: resolve => require(['../components/page/TeacherList.vue'], resolve),
+                    meta: { title: '教师列表' }
+                },
+                {
+                    path: '/studentList',
+                    component: resolve => require(['../components/page/StudentList.vue'], resolve),
+                    meta: { title: '学生列表' }
+                },
+                {
+                    path: '/classList',
+                    component: resolve => require(['../components/page/ClassList.vue'], resolve),
+                    meta: { title: '班级列表' }
+                },
+            ]
+        },
+        {
+            path: '/login',
+            component: resolve => require(['../components/page/Login.vue'], resolve)
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
 })
