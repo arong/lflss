e7bd83b173ea2cada519cbe7aac317c642b8e019
gerrylon <gerrylon@163.com>
2018-11-09 11:24:07 +0800

    add 404 page

---
+++ src/components/common/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="padding-top: 200px; text-align: center;">
+      <h2>
+        页面不存在哦，你可以：
+      </h2>
+      <br>
+      <div>
+       <router-link to="/" replace>返回主页</router-link>
+      </div>
+  </div>
+
+</template>
+<script>
+export default {};
+</script>
