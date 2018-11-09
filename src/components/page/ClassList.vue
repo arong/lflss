2bada96b86d31851c60276c5f3633e091af25aa1
XuRongDong <3003154880@qq.com>
2018-11-09 09:47:53 +0800

    add class list

--- src/components/page/ClassList.vue
+++ src/components/page/ClassList.vue
@@ -7,13 +7,19 @@
         </div>
         <div class="container">
             <div class="handle-box">
-                <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
-                <el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
+                <el-button type="primary" icon="delete" class="handle-del mr10" @click="delList">批量删除</el-button>
+                <el-select v-model.trim = "filter.gender_text" placeholder="筛选性别" class="handle-select mr10">
+                    <el-option key="0" label="全部" value="全部"></el-option>
+                    <el-option key="1" label="男" value='男'></el-option>
+                    <el-option key="2" label="女" value='女'></el-option>
                 </el-select>
-                <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+                <el-input v-model="filter.grade" placeholder="年级" class="handle-select mr10"></el-input>
+                <el-input v-model="filter.index" placeholder="班级" class="handle-select mr10"></el-input>
+                <el-input v-model="filter.name" placeholder="姓名" class="handle-select mr10"></el-input>
+                <el-input v-model="filter.mobile" placeholder="手机号" class="handle-input"></el-input>
+                <el-button type="primary" icon="search" @click="resetSearch">重置</el-button>
                 <el-button type="primary" icon="search" @click="search">搜索</el-button>
+                <el-button type="primary" @click="createClass" class='new-button'>新建</el-button>
             </div>
             <el-table :data="data" border class="table" ref="multipleTable" @selection-change="handleSelectionChange">
                 <el-table-column type="selection" width="30" align="center"></el-table-column>
@@ -34,34 +40,40 @@
                 <el-table-column prop="date" label="入学日期" sortable width="120"> </el-table-column>
                 <el-table-column label="操作" width="180" align="center">
                     <template slot-scope="scope">
-                        <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                        <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index)">编辑</el-button>
                         <el-button type="text" icon="el-icon-delete" class="red" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
             <div class="pagination">
-                <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="1000">
+                <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total=this.total>
                 </el-pagination>
             </div>
         </div>
 
         <!-- 编辑弹出框 -->
-        <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
-            <el-form ref="form" :model="form" label-width="50px">
-                <el-form-item label="日期">
-                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date" value-format="yyyy-MM-dd" style="width: 100%;"></el-date-picker>
+        <el-dialog :title="title" :visible.sync="editVisible" width="30%">
+            <el-form ref="form" :model="form" label-width="100px">
+                <el-form-item label="年级">
+                    <el-input v-model.trim="form.grade"></el-input>
                 </el-form-item>
-                <el-form-item label="姓名">
-                    <el-input v-model="form.name"></el-input>
+                <el-form-item label="班级">
+                    <el-input v-model.trim="form.index"></el-input>
                 </el-form-item>
-                <el-form-item label="地址">
-                    <el-input v-model="form.address"></el-input>
+                <el-form-item label="班主任" prop="subject">
+                  <el-select v-model="form.master_id" placeholder="选择课程">
+                    <el-option
+                      v-for="item in teacher_list"
+                      :key="item.name"
+                      :label="item.name"
+                      :value="item.id">
+                    </el-option>
+                  </el-select>
                 </el-form-item>
-
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="editVisible = false">取 消</el-button>
-                <el-button type="primary" @click="saveEdit">确 定</el-button>
+                <el-button type="primary" @click="saveClass(form.opType)">确 定</el-button>
             </span>
         </el-dialog>
 
@@ -70,30 +82,21 @@
             <div class="del-dialog-cnt">删除不可恢复，是否确定删除？</div>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="delVisible = false">取 消</el-button>
-                <el-button type="primary" @click="deleteRow">确 定</el-button>
+                <el-button type="primary" @click="deleteRow()">确 定</el-button>
             </span>
         </el-dialog>
     </div>
 </template>
 
 <script>
+import utils from "@/utils/index.js";
 export default {
-  name: "basetable",
+  name: "classlist",
   data() {
     return {
       url: "./static/vuetable.json",
-      tableData: [
-        {
-          date: "2018-11-01",
-          mobile: "12345678910",
-          grade:1,
-          index:3,
-          sex: 1,
-          name: "高一三班",
-          address: "nowhere",
-          teacher:'鱼老师'
-        }
-      ],
+      tableData: [],
+      total: 0,
       cur_page: 1,
       multipleSelection: [],
       select_cate: "",
@@ -102,36 +105,35 @@ export default {
       is_search: false,
       editVisible: false,
       delVisible: false,
-      form: {
-        name: "",
-        date: "",
-        address: ""
+      form: {},
+      filter: {},
+      teacher_list: {},
+      idx: -1,
+      title: "",
+      subjectMap: {},
+      subjectReverseMap: {},
+      genderMap: {
+        "0": "未知",
+        "1": "男",
+        "2": "女"
       },
-      idx: -1
+      genderText2Enum: {
+        男: 1,
+        女: 2,
+        未知: 3
+      }
     };
   },
   created() {
-    this.getData();
+    this.getTeacher().then(() => {
+      console.log(this.subjectMap);
+      this.getData();
+    });
   },
   computed: {
     data() {
       return this.tableData.filter(d => {
-        let is_del = false;
-        for (let i = 0; i < this.del_list.length; i++) {
-          if (d.name === this.del_list[i].name) {
-            is_del = true;
-            break;
-          }
-        }
-        if (!is_del) {
-          if (
-            d.address.indexOf(this.select_cate) > -1 &&
-            (d.name.indexOf(this.select_word) > -1 ||
-              d.address.indexOf(this.select_word) > -1)
-          ) {
-            return d;
-          }
-        }
+        return d;
       });
     }
   },
@@ -141,22 +143,43 @@ export default {
       this.cur_page = val;
       this.getData();
     },
-    // 获取 easy-mock 的模拟数据
-    getData() {
-      // 开发环境使用 easy-mock 数据，正式环境使用 json 文件
-      if (process.env.NODE_ENV === "development") {
-        this.url = "/ms/table/list";
+    async getData() {
+      this.filter["page"] = this.cur_page;
+      this.filter["size"] = 10;
+      let res = await utils.simpleGet("/class", this.filter, true);
+      if (res.code === 0) {
+        this.tableData = res.data.list;
+        this.total = res.data.total;
+      } else {
+        this.tableData = [];
       }
-      this.$axios
-        .post(this.url, {
-          page: this.cur_page
-        })
-        .then(res => {
-          this.tableData = res.data.list;
-        });
+    },
+    async getTeacher() {
+      let res = await utils.simpleGet("/teacher/", {}, true);
+      if (res.code === 0) {
+        this.teacher_list = res.data.list
+      } else {
+        this.subjectMap = {};
+      }
+    },
+    newTeacher() {
+      return {
+        grade: 0,
+        index: 0,
+        name: ""
+      };
+    },
+    newForm(type) {
+      let data = this.newTeacher();
+      data["opType"] = type;
+      return data;
+    },
+    resetSearch() {
+      this.filter = {};
     },
     search() {
-      this.is_search = true;
+      this.cur_page = 1;
+      this.getData();
     },
     formatter(row, column) {
       return row.address;
@@ -164,44 +187,112 @@ export default {
     filterTag(value, row) {
       return row.tag === value;
     },
-    handleEdit(index, row) {
-      this.idx = index;
-      const item = this.tableData[index];
-      this.form = {
-        name: item.name,
-        date: item.date,
-        address: item.address
-      };
+    createClass() {
+      this.title = "新建";
+      this.form = this.newForm("create");
       this.editVisible = true;
+    },
+    async handleEdit(index) {
+      this.title = "编辑";
+      this.editVisible = true;
+      // let res = await utils.simpleGet("/teacher/", {}, true);
+      // if (res.code == 0) {
+      //   this.teacher_list = res.data.list
+      // } else {
+      //   this.$message.error(res.msg);
+      //   return;
+      // }
+      this.form["opType"] = "edit";
     },
     handleDelete(index, row) {
       this.idx = index;
       this.delVisible = true;
     },
-    delAll() {
-      const length = this.multipleSelection.length;
-      let str = "";
-      this.del_list = this.del_list.concat(this.multipleSelection);
-      for (let i = 0; i < length; i++) {
-        str += this.multipleSelection[i].name + " ";
-      }
-      this.$message.error("删除了" + str);
-      this.multipleSelection = [];
-    },
     handleSelectionChange(val) {
       this.multipleSelection = val;
     },
-    // 保存编辑
-    saveEdit() {
-      this.$set(this.tableData, this.idx, this.form);
-      this.editVisible = false;
-      this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+    checkFormData(data) {
+      if (data.opType == "create") {
+      }
+      if (data.grade == 0) {
+        this.$message.error("年级为必填");
+        return false;
+      }
+      data.grade = Number(data.grade);
+
+      if (data.index == 0) {
+        this.$message.error("班级编号为必填");
+        return false;
+      }
+      data.index = Number(data.index);
+      return true;
     },
-    // 确定删除
-    deleteRow() {
-      this.tableData.splice(this.idx, 1);
-      this.$message.success("删除成功");
+    // 保存编辑
+    async saveClass(opType) {
+      let isValid = this.checkFormData(this.form);
+      if (!isValid) {
+        return;
+      }
+      if (opType == "edit") {
+        let res = await utils.simplePut(
+          "/class/" + this.form.teacher_id,
+          this.form,
+          true
+        );
+        if (res.code == 0) {
+          this.$message.success("修改成功");
+        } else {
+          this.$message.error(res.msg);
+          return;
+        }
+      } else if (opType == "create") {
+        let res = await utils.simplePost("/class", this.form, true);
+        if (res.code === 0) {
+          this.$message.success("添加成功");
+        } else {
+          this.$message.error(res.msg);
+          return;
+        }
+      }
+      this.editVisible = false;
+      this.getData();
+    },
+    async deleteRow() {
+      let id = this.tableData[this.idx].teacher_id;
+      let res = await utils.simplePost(
+        "/teacher/delete",
+        { id_list: [id] },
+        true
+      );
+      if (res.code === 0) {
+        this.$message.success("删除成功");
+      } else {
+        this.$message.error(res.msg);
+        return;
+      }
       this.delVisible = false;
+      this.getData();
+    },
+    async delList() {
+      let ids = [];
+      for (var i = 0; i < this.multipleSelection.length; i++) {
+        ids.push(this.multipleSelection[i].teacher_id);
+      }
+      if (ids.length == 0) {
+        return;
+      }
+      let res = await utils.simplePost(
+        "/teacher/delete",
+        { id_list: ids },
+        true
+      );
+      if (res.code === 0) {
+        this.$message.success("删除成功");
+      } else {
+        this.$message.error(res.msg);
+        return;
+      }
+      this.getData();
     }
   }
 };
