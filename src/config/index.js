a5e5d815aa2bb3615bf34a030ff10cc0bcf0dfb6
XuRongDong <3003154880@qq.com>
2018-11-10 15:39:14 +0800

    change host

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,5 @@
-// 要与proxyTable中的保持一致
-export const PROXY_TABLE_PREFIX = '/api';
-
 // 主机
-export const HOST = 'http://localhost:2008';
+export const HOST = 'https://www.lzlflss.com';
 // 固定的请求头
 export const FIXED_PATH = '/api/v1/dean';
 // 请求前缀
